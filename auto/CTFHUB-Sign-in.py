8755029369eb1b609195ed9950d2422b32c06bf9
Zoey Zhao <46625971+hellolux@users.noreply.github.com>
2020-11-20 14:13:20 +0800

    CTFHUB自动签到

---
+++ auto/CTFHUB-Sign-in.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author  : Zoey Zhao
+# @Time    : 2020/11/20 11:40
+# @Function: CTFHUB自动签到
+
+import requests
+
+url = "https://api.ctfhub.com/User_API/User/checkIn"
+
+payload = "{}"
+headers = {
+  'Authorization': 'ctfhub_sessid=eqo5e95akqpluloeef4th18uc28akooc',
+  'Content-Type': 'application/json;charset=UTF-8',
+}
+
+response = requests.request("POST", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+
+
